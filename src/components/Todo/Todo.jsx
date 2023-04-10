452c7a7396af0fd18bba984a1263ccb5762ba5f9
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-04-10 18:02:32 +0430

    Edit style and html

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,14 +3,19 @@ import styles from "./todo.module.css";
 const Todo = () => {
   return (
     <div className={styles.todo}>
-      <div className={styles.title}>
-        <h4>Todo</h4>
+      <div className={styles.info}>
+        <div className={styles.title}>
+          <h4>Todo</h4>
+        </div>
+        <div className={styles.btnBox}>
+          <button type="button">C</button>
+          <button type="button">E</button>
+          <button type="button">D</button>
+        </div>
       </div>
-      <div className={styles.btnBox}>
-        <div className={styles.createdAt}>4/10/2023</div>
-        <button type="button">C</button>
-        <button type="button">E</button>
-        <button type="button">D</button>
+      <div className={styles.date}>
+        <div className={styles.createdAt}>Created at:4/10/2023</div>
+        <div className={styles.createdAt}>Last Update:4/11/2023</div>
       </div>
     </div>
   );
