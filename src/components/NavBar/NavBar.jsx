09a78a030ab91e8d6105bddab2f53a71a64bbdea
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-04-13 13:14:53 +0430

    navbar comp styles changed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,24 @@
+import { FaPlus } from "react-icons/fa";
 import styles from "./navBar.module.css";
 
 const NavBar = () => {
   return (
     <header className={styles.navBar}>
-      <h1>TodoList App</h1>
+      <div className={styles.searchBox}>
+        <input
+          type="text"
+          placeholder="Search Todos..."
+          className={styles.search}
+        />
+      </div>
+      <div className={styles.title}>
+        <h1>To Do List App</h1>
+      </div>
+      <div className={styles.addBtnBox}>
+        <button type="button" className={styles.addBtn}>
+          <FaPlus /> <span>Create</span>
+        </button>
+      </div>
     </header>
   );
 };
